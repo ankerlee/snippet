ae46ef6a99657bbb090b583ad82cc5d3f030d49f
anker <ankerlee@qq.com>
2014-08-27 17:34:42 +0800

    初始工程

---
+++ snippet/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  snippet
+//
+//  Created by lili on 14-8-27.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ snippetTests/snippetTests.m
@@ -0,0 +1,34 @@
+//
+//  snippetTests.m
+//  snippetTests
+//
+//  Created by lili on 14-8-27.
+//
+//
+
+#import <XCTest/XCTest.h>
+
+@interface snippetTests : XCTestCase
+
+@end
+
+@implementation snippetTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
