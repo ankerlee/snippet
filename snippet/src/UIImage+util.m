e96a7cb828d2df890010d978a4a05380201c79c4
anker <ankerlee@qq.com>
2014-09-17 16:43:20 +0800

    测试pod版本管理

--- snippet/src/UIImage+util.m
+++ snippet/src/UIImage+util.m
@@ -14,13 +14,10 @@
     NSParameterAssert(maskColor != nil);
     
     CGRect imageRect = CGRectMake(0.0f, 0.0f, self.size.width, self.size.height);
-    
     UIGraphicsBeginImageContextWithOptions(imageRect.size, NO, self.scale);
     CGContextRef context = UIGraphicsGetCurrentContext();
-    
     CGContextScaleCTM(context, 1.0f, -1.0f);
     CGContextTranslateCTM(context, 0.0f, -(imageRect.size.height));
-    
     CGContextClipToMask(context, imageRect, self.CGImage);
     CGContextSetFillColorWithColor(context, maskColor.CGColor);
     CGContextFillRect(context, imageRect);
